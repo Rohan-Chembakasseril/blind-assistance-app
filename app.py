7cb710fb25a4a1e35b99318886d8fb3f6c571e6d
Rohan Chembakasseril <75856879+Rohan-Chembakasseril@users.noreply.github.com>
2024-05-12 11:25:52 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def text_to_speech(text):
 st.title("Blind Assistance System")
 st.write("---")
 # Take inputs from the streamlit
-model_name = st.sidebar.selectbox("Select Pre-trained Model:", ("BLIP-Large", "BLIP-Base", "VIT-GPT-2"))
+model_name = st.sidebar.selectbox("Select Pre-trained Model:", ("BLIP-Large", "BLIP-Base", "VIT-GPT-2", "BLIP-Long"))
 st.write("The purpose of this project is to build a blind assistance system using custom or existing pre-trained models. I have implemented it using 3 different models.")
 st.write("1]VIT-GPT2: The Vision Encoder Decoder Model has been used to initialize an image-to-text model using VIT as the encoder and the pre-trained language model GPT2 as the decoder.")
 st.write("2]BLIP: Bootstrapping Language-Image Pre-training for Unified Vision-Language Understanding and Generation: a)Large and b) Base. BLIP is a new Vision Language Processing framework which transfers flexibly to both vision-language understanding and generation tasks. BLIP effectively utilizes the noisy web data by bootstrapping the captions, where a captioner generates synthetic captions and a filter removes the noisy ones.")
@@ -80,6 +80,23 @@ if uploaded_file is not None:
         # text_to_speech(image_to_text(image)[0]["generated_text"])
         st.write(processor_blip_large.decode(out[0], skip_special_tokens=True))
         text_to_speech(processor_blip_large.decode(out[0], skip_special_tokens=True))
+    elif model_name == "BLIP-Long":
+        st.write("BLIP-Long model is selected.")
+        # image_to_text = pipeline("image-to-text", model="Salesforce/blip-image-captioning-large")
+
+        processor_blip_large = BlipProcessor.from_pretrained(
+            "unography/blip-large-long-cap"
+        )
+        model_blip_large = BlipForConditionalGeneration.from_pretrained(
+            "unography/blip-large-long-cap"
+        )
+        image = Image.open(uploaded_file)
+        st.image(image, caption="Uploaded Image.", use_column_width=True)
+        inputs = processor_blip_large(image, return_tensors="pt")
+        pixel_values = inputs.pixel_values
+        out = model_blip_large.generate(pixel_values=pixel_values, max_length=250)
+        st.write(processor_blip_large.decode(out[0], skip_special_tokens=True))
+        text_to_speech(processor_blip_large.decode(out[0], skip_special_tokens=True))
     else:
         st.write("BLIP-Base model is selected.")
         # image_to_text = pipeline("image-to-text", model="Salesforce/blip-image-captioning-large")
@@ -96,4 +113,3 @@ if uploaded_file is not None:
         out = model_blip.generate(**inputs)
         st.write(processor_blip.decode(out[0], skip_special_tokens=True))
         text_to_speech(processor_blip.decode(out[0], skip_special_tokens=True))
-        
